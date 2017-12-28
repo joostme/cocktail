949d5e539d4225375366147b6422d50791ce3abc
Joost Zöllner <joostzoellner@online.de>
2017-12-28 18:08:59 +0100

    Some stuff related to favorites that were missing in the commit

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ctl-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'ctl-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'ctl';
 }
