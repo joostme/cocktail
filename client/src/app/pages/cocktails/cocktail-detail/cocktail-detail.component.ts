b057326954819e86c30b13632f721d550727ac22
Joost Zöllner <joostzoellner@online.de>
2017-12-30 13:56:55 +0100

    Moved select cocktail to resolver

--- client/src/app/pages/cocktails/cocktail-detail/cocktail-detail.component.ts
+++ client/src/app/pages/cocktails/cocktail-detail/cocktail-detail.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Cocktail } from '../../../shared/cocktails/cocktails.model';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../../store/app-state';
 import { Observable } from 'rxjs/Observable';
 import { selectCurrentCocktail } from '../../../store/cocktails/cocktails.selectors';
-import { SelectCocktailAction } from '../../../store/cocktails/cocktails.actions';
 
 @Component({
     selector: 'ctl-cocktail-detail',
@@ -18,10 +17,8 @@ export class CocktailDetailComponent {
     constructor(
         private router: Router,
         private store: Store<AppState>,
-        private activatedRoute: ActivatedRoute
 
     ) {
-        this.store.dispatch(new SelectCocktailAction(activatedRoute.snapshot.params.id));
         this.cocktail = this.store.select(selectCurrentCocktail);
     }
 
